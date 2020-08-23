8b7a2c8172d3ff1d0c3a95af4a9cfab7b8187b42
yassataiseer <56459253+yassataiseer@users.noreply.github.com>
2020-08-23 01:15:20 -0400

    Add files via upload

--- test.py
+++ test.py
@@ -1,16 +1,40 @@
-a = input()
-global email
+import sqlite3
+import time
+import datetime
+import random
 
-def test(a):
-    print("var",a)
-    global email
-    email =a
-    print(email)
+conn = sqlite3.connect('database.db', check_same_thread=False)
+c = conn.cursor()
 
-def test1():
-    global email
-    email+="hello"
-    print(email)
 
-test(a)
-test1()
+
+
+class data_answer:
+    def data():
+        send_data=[]
+        c.execute('SELECT * FROM User')
+        data = c.fetchall()
+        #print(data)
+        for rows in data:
+            minor_data=[]
+            email = rows[1]
+            bday = rows[3]
+            server = rows[4]
+            timing = rows[5]
+            name = rows[-1]
+            about = rows[-2]
+            minor_data.append(name)
+            minor_data.append(timing)
+            minor_data.append(email)
+            minor_data.append(server)
+            minor_data.append(bday)
+            minor_data.append(about)
+            send_data.append(minor_data)
+        return( send_data)
+
+            
+
+
+
+
+#print(data_answer.data())
